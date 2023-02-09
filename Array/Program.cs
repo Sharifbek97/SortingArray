1287ff4270b8315072e3f082c2d62522d92ee945
Sharifbek97 <sharifbekmirzozoda2@gmail.com>
2023-02-09 21:38:36 +0500

    Added sorting array

---
+++ Array/Program.cs
@@ -0,0 +1,21 @@
+﻿
+ var numbers=new[] {2,10,-3,3,24,53,12};
+ var count = numbers.Length;
+ 
+ for (var i = 0; i <count ; ++i)
+ {
+     Console.Write(numbers[i]+"   ");
+ }
+ Console.Write("\n");
+ for (var i = 0; i < count; ++i)
+ {
+     for (var j = i + 1; j < count; j++)
+     {
+         if (numbers[i] < numbers[j])
+         {
+             (numbers[i], numbers[j]) = (numbers[j], numbers[i]);
+         } 
+
+     }Console.Write("  " + numbers[i]);
+ }
+ 
